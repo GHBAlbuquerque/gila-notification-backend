402b0c16c209bbdd46f11b626bec6261f233ed71
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-07-24 21:34:57 -0300

    feat: adds annotations on controller

--- src/main/java/gila/notification/adapters/controllers/NotificationController.java
+++ src/main/java/gila/notification/adapters/controllers/NotificationController.java
@@ -1,4 +1,11 @@
 package gila.notification.adapters.controllers;
 
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequestMapping("/notifications")
 public class NotificationController {
 }
